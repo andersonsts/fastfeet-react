d23c658dea9d88d51c87d2eb4ae852ec05a94478
Anderson <andersonst.dev@gmail.com>
2020-03-31 14:32:15 -0300

    Navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaSignOutAlt } from 'react-icons/fa';
+
+import avatar from '../../assets/avatar.jpg';
 
 import { Container, Content } from './styles';
 
@@ -6,7 +10,25 @@ export default function Header() {
   return (
     <Container>
       <Content>
-        <h1>Header</h1>
+        <nav>
+          <Link to="/">
+            <img src="" alt="" />
+          </Link>
+          <Link to="/">Encomendas</Link>
+          <Link to="/">Entregadores</Link>
+          <Link to="/">Destinatários</Link>
+        </nav>
+
+        <aside>
+          <img src={avatar} alt="Avatar" />
+          <div>
+            <strong>Admin FastFeet</strong>
+            <Link to="/logout">
+              <FaSignOutAlt />
+              <p>Sair</p>
+            </Link>
+          </div>
+        </aside>
       </Content>
     </Container>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,68 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #444;
+  padding: 0 30px;
+`;
+
+export const Content = styled.div`
+  padding: 0px 20px;
+  height: 64px;
+  background: #eee;
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  nav {
+    display: flex;
+    align-items: center;
+    height: 100%;
+
+    a {
+      background: #777;
+      display: flex;
+      padding: 0 20px;
+      align-items: center;
+      border-bottom: 5px solid black;
+      height: 100%;
+    }
+  }
+
+  aside {
+    display: flex;
+    align-items: center;
+    font-family: 'Roboto';
+
+    img {
+      box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
+      width: 40px;
+      height: 40px;
+      margin-right: 10px;
+      border-radius: 50%;
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+
+      strong {
+        font-size: 15px;
+      }
+
+      a {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+        color: red;
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+    }
+  }
+`;
