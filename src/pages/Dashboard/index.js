b3c9fb944c993e82028a69958bc013ef0b99eb38
Anderson <andersonst.dev@gmail.com>
2020-04-01 23:20:59 -0300

    Titulo da tabela

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { MdSearch } from 'react-icons/md';
+import { FaPlus } from 'react-icons/fa';
 
-import { Container } from './styles';
+import { Container, Top } from './styles';
 
 export default function Dashboard() {
   return (
     <Container>
       <h2>Gerenciando encomendas</h2>
-      <div>
-        <MdSearch color="#999" />
-        <input type="text" />
-      </div>
+      <Top>
+        <div>
+          <button type="button">
+            <MdSearch />
+          </button>
+          <input type="text" />
+        </div>
+
+        <button type="button">
+          <FaPlus />
+          <strong>CADASTRAR</strong>
+        </button>
+      </Top>
     </Container>
   );
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,32 +1,91 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.div`
-  max-width: 1100px;
+  max-width: 1200px;
   width: 100%;
   margin: 35px auto;
 
   h2 {
     font-size: 24px;
+    font-weight: bold;
+    width: 400px;
+    text-transform: uppercase;
+    color: #444;
   }
+`;
+
+export const Top = styled.div`
+  margin-top: 35px;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
   div {
-    position: relative;
-    margin-top: 35px;
-    margin-bottom: 20px;
+    width: 237px;
+    height: 36px;
+    display: flex;
+    align-items: center;
+    background: #fff;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    box-shadow: 2px 2px 5px rgba(1, 1, 1, 0.1);
+    padding-right: 25px;
 
     input {
-      width: 240px;
-      padding: 10px 45px;
-      color: #999;
+      width: 100%;
       font-size: 14px;
-      border: 1px solid #ddd;
-      border-radius: 4px;
+      height: 90%;
+      border: none;
+      color: #999;
+    }
+
+    button {
+      background: none;
+      border: 0;
+      display: flex;
+      align-items: center;
+      margin: 0px 8px 0px 16px;
+
+      &:hover svg {
+        color: #7159c1;
+      }
+
+      svg {
+        transition: all 0.18s ease-in-out;
+        font-size: 25px;
+        color: #999;
+      }
+    }
+  }
+
+  > button {
+    background: #7d40e7;
+    width: 142px;
+    height: 36px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    border-radius: 4px;
+    padding: 8px 18px;
+    transition: all 0.25s ease-in-out;
+
+    &:hover {
+      background: ${darken(0.15, '#7d40e7')};
+    }
+
+    strong {
+      font-size: 14px;
+      font-weight: bold;
+      color: #fff;
     }
 
     svg {
-      position: absolute;
-      top: 10px;
-      left: 10px;
+      color: #fff;
+      font-size: 18px;
+      margin-right: 8px;
     }
   }
 `;
